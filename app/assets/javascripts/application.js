7318a5c661599352b0802fddad1b5631b2e0a9f0
Frederick Wong <frederick.kf.wong@gmail.com>
2015-02-05 22:09:16 +1100

    Set up backbone environment -folders and dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require handlebars.runtime
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./templates
+//= require_tree ./views
 //= require_tree .
