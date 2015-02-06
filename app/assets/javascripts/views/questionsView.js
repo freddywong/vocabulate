955c33d4b84f260275b8381a72f406b69ab09e9f
Frederick Wong <frederick.kf.wong@gmail.com>
2015-02-06 20:05:18 +1100

    Questions now show up in correct order

--- app/assets/javascripts/views/questionsView.js
+++ app/assets/javascripts/views/questionsView.js
@@ -10,11 +10,36 @@ var App = App || {};
     },
 
     render: function() {
-      this.$el.html(
-        HandlebarsTemplates['questions/index']({ questions: App.questionsCollection.toJSON() })
-      );
 
+      var questionsSequence = App.questionsCollection.sortBy(function(question) {
+        return question.get("times_correct");
+      });
+
+      var 
+
+      this.$el.html(
+        HandlebarsTemplates['questions/index']({ questions: questionsSequence })
+      );
       return this;
     }
   });
 })(App);
+
+
+    // render: function(){
+
+    //   var flightsWithPlanes = [];
+    //   App.flightsCollection.each(function(flight) {
+    //     flight.attributes.formattedDate = flight.attributes.date.substr(0, 10);
+    //     var withPlane = flight.toJSON();
+    //     withPlane.planeName = App.planesCollection.get(flight.get('plane_id')).get('name');
+    //     flightsWithPlanes.push(withPlane)
+    //   });
+
+    //   this.$el.html(
+    //     HandlebarsTemplates['flights/index']({ flights: flightsWithPlanes })
+    //     );
+
+
+      // return this;
+    // }
