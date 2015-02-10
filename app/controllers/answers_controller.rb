dda159199b98de480a6d26853174b0d15df162b8
Frederick Wong <frederick.kf.wong@gmail.com>
2015-02-10 12:33:53 +1100

    User must sign in to use app

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  before_action :authenticate_user!
   def index
     render json: Answer.all
   end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!
   before_action :find_question, only:[ :update]
   def index
     render json: Question.all
