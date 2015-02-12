e18e335996a54ba9f5d3bc74ee68dee0a372e94d
Frederick Wong <frederick.kf.wong@gmail.com>
2015-02-13 10:04:35 +1100

    Login now goes to my quizzes page + views transition in between each other

--- app/assets/javascripts/views/appView.js
+++ app/assets/javascripts/views/appView.js
@@ -14,77 +14,88 @@ var App = App || {};
 
     displayUserQuizzes: function(View) {
       this.currentView = new View();
-      this.$el.find(".app-content").html(this.currentView.render().el);
-      
-      this.$el.find(".user-quiz").each(function(){
+      this.$el.find(".app-content").fadeOut(function(){
+        this.$el.find(".app-content").html(this.currentView.render().el).fadeIn();
+        this.$el.find(".user-quiz").each(function(){
 
-      var progressAmount = (2 * $(this).find(".progress-amount").attr("data-progress"));
+          var progressAmount = (2 * $(this).find(".progress-amount").attr("data-progress"));
 
-      $(this).find(".progress-bar").animate({
-          width: progressAmount + "%"
-        }, 2500);
+          $(this).find(".progress-bar").animate({
+              width: progressAmount + "%"
+            }, 2500);
 
-      
-      var percent_number_step = $.animateNumber.numberStepFactories.append(' %')
-      $(this).find(".progress-bar").animateNumber(
-        {
-          number: progressAmount,
-          color: 'orange',
-          'font-size': '30px',
+          
+          var percent_number_step = $.animateNumber.numberStepFactories.append(' %')
+          $(this).find(".progress-bar").animateNumber(
+            {
+              number: progressAmount,
+              color: 'orange',
+              'font-size': '30px',
 
-          easing: 'easeInQuad',
+              easing: 'easeInQuad',
 
-          numberStep: percent_number_step
-        },
-        2500
-      );
+              numberStep: percent_number_step
+            },
+            2500
+          );
 
-    });
+        });
+      }.bind(this))
     },
 
     displayUserQuizOverview: function(View, userQuizId) {
       this.currentView = new View();
-      this.$el.find(".app-content").html(this.currentView.render(userQuizId).el);
+      this.$el.find(".app-content").fadeOut(function(){
+        this.$el.find(".app-content").html(this.currentView.render(userQuizId).el).fadeIn();
+      }.bind(this)) 
     },
 
     displayQuestion: function(View) {
       this.currentView = new View();
-      this.$el.find(".app-content").html(this.currentView.render().el);
-      
-      this.$el.find(".results-progress").each(function(){
+      this.$el.find(".app-content").fadeOut(function() {
+        this.$el.find(".app-content").html(this.currentView.render().el).fadeIn();
+          this.$el.find(".results-progress").each(function(){
 
-        var progressAmount = (20 * $(this).find(".times-correct-amount").attr("data-times-correct"));
+          var progressAmount = (20 * $(this).find(".times-correct-amount").attr("data-times-correct"));
 
-        $(this).find(".progress-bar").animate({
-            width: progressAmount + "%"
-          }, 2500);
+          $(this).find(".progress-bar").animate({
+              width: progressAmount + "%"
+            }, 2500);
 
-        
-        var percent_number_step = $.animateNumber.numberStepFactories.append(' %')
-        $(this).find(".progress-bar").animateNumber(
-          {
-            number: progressAmount,
-            color: 'orange',
-            'font-size': '20px',
+          
+          var percent_number_step = $.animateNumber.numberStepFactories.append(' %')
+          $(this).find(".progress-bar").animateNumber(
+            {
+              number: progressAmount,
+              color: 'orange',
+              'font-size': '20px',
 
-            easing: 'easeInQuad',
+              easing: 'easeInQuad',
 
-            numberStep: percent_number_step
-          },
-          2500
-        );
+              numberStep: percent_number_step
+            },
+            2500
+          );
 
-      });
+        });
+      }.bind(this)); 
+
     },
 
     displayAnswers: function(View, questionId, userQuizId) {
       this.currentView = new View();
-      this.$el.find(".app-content").html(this.currentView.render(questionId, userQuizId).el);
+      this.$el.find(".app-content").fadeOut(function(){
+        this.$el.find(".app-content").html(this.currentView.render(questionId, userQuizId).el).fadeIn();  
+      }.bind(this))
+      
     },
 
     displayAnswer: function(View, questionId, answerId) {
       this.currentView = new View();
-      this.$el.find(".app-content").html(this.currentView.render(questionId, answerId).el);
+      this.$el.find(".app-content").fadeOut(function(){
+        this.$el.find(".app-content").html(this.currentView.render(questionId, answerId).el).fadeIn();  
+      }.bind(this))
+      
     }
 
   });
