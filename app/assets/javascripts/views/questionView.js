e6d01cad0b96fcf71fa105313c2f5b49aca0b1ff
Frederick Wong <frederick.kf.wong@gmail.com>
2015-02-12 20:00:12 +1100

    Attempting to create a superCollection of Questions and Answers for results template

--- app/assets/javascripts/views/questionView.js
+++ app/assets/javascripts/views/questionView.js
@@ -27,6 +27,7 @@ var App = App || {};
         $(".app-content").attr("data-sequence", sequenceNumber);
 
       } else {
+        $.extend(App.questionsCollection.toJSON(), App.answersCollection.toJSON())
 
         this.$el.html(
           HandlebarsTemplates['questions/results']({ questions: App.quizQuestions.toJSON(), answers: App.quizAnswers.toJSON() })
